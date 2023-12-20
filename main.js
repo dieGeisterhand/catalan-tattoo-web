ae447bab9881ceb2f928fdc2df185c1879d8d6d4
Jeyfrem <jeyfrem@proton.me>
2023-12-20 16:00:11 -0500

    oopsie

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 
 // Populate array with images
 const image_arr =
-  [ "1.jpeg"
-  , "2.jpeg"
-  , "3.jpeg"
-  , "4.jpeg"
-  , "5.jpeg"
-  , "6.jpeg"
-  , "7.jpeg"
+  [ "1.jpg"
+  , "2.jpg"
+  , "3.jpg"
+  , "4.jpg"
+  , "5.jpg"
+  , "6.jpg"
+  , "7.jpg"
     ];
 const image_count = image_arr.length;
 
