d709868bfc9ecaea30c696b7c674dff40955dd0f
Jeyfrem <jeyfrem@proton.me>
2023-12-20 15:58:18 -0500

    adding assets

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
 
 // Populate array with images
 const image_arr =
-  [ "https://static.zerochan.net/Touhou.full.467303.jpg"
-  , "https://i.pinimg.com/originals/b7/c2/42/b7c24261e7e37c0bdfd58307fb199891.jpg"
-  , "https://pixelz.cc/wp-content/uploads/2018/12/touhou-reimu-hakurei-uhd-4k-wallpaper.jpg"
-  , "https://images6.fanpop.com/image/photos/40500000/Sakuya-Izayoi-touhou-40555313-1010-1400.jpg"
-  , "https://images4.alphacoders.com/230/230284.jpg"
-  , "https://static.zerochan.net/Touhou.full.878183.jpg"
-  , "https://images6.alphacoders.com/762/762217.jpg"
-  , "https://images2.alphacoders.com/871/thumb-1920-871561.jpg"
-  , "https://static.zerochan.net/Touhou.full.1241244.jpg"
+  [ "1.jpeg"
+  , "2.jpeg"
+  , "3.jpeg"
+  , "4.jpeg"
+  , "5.jpeg"
+  , "6.jpeg"
+  , "7.jpeg"
     ];
 const image_count = image_arr.length;
 
